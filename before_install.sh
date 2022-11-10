041b3956e8cdc3f1f8f7d24ab2f40b6e8a5663e1
jaff-it <serg.sayfulin@gmail.com>
2022-11-10 10:47:34 +0200

    Add script before_install

--- before_install.sh
+++ before_install.sh
@@ -17,4 +17,11 @@ echo "ECR_IMAGE_TAG: $ECR_IMAGE_TAG"
 aws ecr get-login-password --region $AWS_DEFAULT_REGION| docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$ECR_REPO_NAME:$ECR_IMAGE_TAG
 
-docker run -p 8385:80 $ECR_REPO_NAME:$ECR_IMAGE_TAG
+
+CONTAINER=$(docker ps -f name=2048 -q)
+if [[ $(echo $CONTAINER | wc -m) != 0 ]]
+then
+  docker stop $CONTAINER
+fi
+
+docker run -d --rm --name 2048 -p 8385:80 $ECR_REPO_NAME:$ECR_IMAGE_TAG
