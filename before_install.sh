c5ab8d322e5e84ca92cd7df3bd36e192094511ad
jaff-it <serg.sayfulin@gmail.com>
2022-11-10 11:38:41 +0200

    Change deploy configuration

--- before_install.sh
+++ before_install.sh
@@ -17,7 +17,6 @@ echo "ECR_IMAGE_TAG: $ECR_IMAGE_TAG"
 aws ecr get-login-password --region $AWS_DEFAULT_REGION| docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$ECR_REPO_NAME:$ECR_IMAGE_TAG
 
-
 CONTAINER=$(docker ps -f name=2048 -q)
 if [[ $(echo $CONTAINER | wc -m) != 0 ]]
 then
